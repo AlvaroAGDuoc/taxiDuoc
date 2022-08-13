e8e591a91a0caa38ea41c3bca8f1236af552fd36
AlvaroAGDuoc <a.aguerog@duocuc.cl>
2022-08-13 13:21:00 -0400

    Comienzo del diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,50 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: 'menu-inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'menu-inicio',
+    loadChildren: () =>
+      import('./pages/menu-inicio/menu-inicio.module').then(
+        (m) => m.MenuInicioPageModule
+      ),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
+    path: 'registro',
+    loadChildren: () =>
+      import('./pages/registro/registro.module').then(
+        (m) => m.RegistroPageModule
+      ),
+  },
+  {
+    path: 'pantalla-principal',
+    loadChildren: () =>
+      import('./pages/pantalla-principal/pantalla-principal.module').then(
+        (m) => m.PantallaPrincipalPageModule
+      ),
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
