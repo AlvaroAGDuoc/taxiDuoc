47ade384dde18a455523d0a3dbd9ff53d3c3d3f9
AlvaroAGDuoc <a.aguerog@duocuc.cl>
2022-08-24 16:56:09 -0400

    subir foto listo

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-registro',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
+
+  imagenSeleccionada: any;
+
+
   constructor() { }
+
+  checkPlatformForWeb() {
+    if (Capacitor.getPlatform() == 'web') return true;
+    return false;
+  }
+
+
+ async obtenerFoto() {
+    const imagen = await Camera.getPhoto({
+      quality: 90,
+      width: 200,
+      height: 200,
+      source: CameraSource.Photos,
+      resultType: this.checkPlatformForWeb() ? CameraResultType.DataUrl: CameraResultType.Uri
+    });
+    this.imagenSeleccionada = imagen;
+    if(this.checkPlatformForWeb()) this.imagenSeleccionada.webPath = imagen.dataUrl;
+
+  }
 
   ngOnInit() {
   }
